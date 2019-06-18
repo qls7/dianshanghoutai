388f4aaf57f0e02a7bb45ddc53b3ace3fc32e8c9
qls7 <qls7@protonmail.com>
2019-06-18 15:10:02 +0800

    获取网站日新增用户的API接口

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 
-from meiduo_admin.views.statistical import StatisticalTotalCountView
+from meiduo_admin.views.statistical import StatisticalTotalCountView, StatisticalDayIncrementView
 from meiduo_admin.views.users import AuthorizationView
 
 urlpatterns = [
     url(r'^authorizations/$', AuthorizationView().as_view()),
     url(r'^statistical/total_count/$', StatisticalTotalCountView.as_view()),
+    url(r'^statistical/day_increment/$', StatisticalDayIncrementView.as_view()),
 ]
