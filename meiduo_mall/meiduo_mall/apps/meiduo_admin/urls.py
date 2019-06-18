92c8a0d077cd1c62961589206de5e4312e7c28dd
qls7 <qls7@protonmail.com>
2019-06-18 16:53:43 +0800

    获取网站日活跃用户的API接口

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 
-from meiduo_admin.views.statistical import StatisticalTotalCountView, StatisticalDayIncrementView
+from meiduo_admin.views.statistical import *
 from meiduo_admin.views.users import AuthorizationView
 
 urlpatterns = [
     url(r'^authorizations/$', AuthorizationView().as_view()),
     url(r'^statistical/total_count/$', StatisticalTotalCountView.as_view()),
     url(r'^statistical/day_increment/$', StatisticalDayIncrementView.as_view()),
+    url(r'^statistical/day_active/$', StatisticalDayActiveView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
@@ -6,6 +6,20 @@ from rest_framework.views import APIView
 from users.models import User
 
 
+
+
+
+# GET /meiduo_admin/statistical/day_active/
+class StatisticalDayActiveView(APIView):
+    """获取网站日活跃用户"""
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        count = User.objects.filter(last_login__gt=date, is_staff=False).count()
+        return Response({'count': count, 'date': date.date()})
+
+
 # GET /meiduo_admin/statistical/day_increment/
 class StatisticalDayIncrementView(APIView):
     """获取网站日新增用户"""
@@ -14,7 +28,7 @@ class StatisticalDayIncrementView(APIView):
     def get(self, request):
         date = timezone.now().replace(hour=0, microsecond=0, minute=0, second=0)
         count = User.objects.filter(date_joined__gt=date, is_staff=False).count()
-        return Response({'count':count, 'date': date})
+        return Response({'count':count, 'date': date.date()})
 
 
 # GET /meiduo_admin/statistical/total_count/
