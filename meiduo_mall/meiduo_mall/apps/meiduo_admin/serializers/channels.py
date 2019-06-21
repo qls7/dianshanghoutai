61cebd4bcd417f18fe392de84944a9153780142c
qls7 <qls7@protonmail.com>
2019-06-21 20:00:16 +0800

    新增网站商品频道数据的API接口

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/channels.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/channels.py
@@ -23,8 +23,26 @@ class GoodsChannelSerializer(serializers.ModelSerializer):
     """商品频道序列化器类"""
     category = serializers.StringRelatedField(label='一级分类名称')
     group = serializers.StringRelatedField(label='频道组名称')
+    # 这里要重新指定category_id和group_id, 默认的的是readonlyField不支持反序列化
+    category_id = serializers.IntegerField(label='一级分类id')
+    group_id = serializers.IntegerField(label='频道id')
 
     class Meta:
         model = GoodsChannel
         fields = ('id', 'category_id', 'group_id', 'url', 'sequence',
                   'category', 'group')
+
+    def validate(self, attrs):
+        """校验传过来的频道组id和一级分类的id"""
+        category_id = attrs['category_id']
+        group_id = attrs['group_id']
+        count = GoodsCategory.objects.filter(id=category_id, parent=None).count()
+        if count <= 0:
+            raise serializers.ValidationError('一级分类id错误')
+        count = GoodsChannelGroup.objects.filter(id=group_id).count()
+        if count <= 0:
+            raise serializers.ValidationError('频道id错误')
+
+        return attrs
+
+
